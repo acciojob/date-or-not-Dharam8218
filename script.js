6ed96b9f4504b168d1ad8739e3076c32ad019363
Dharam8218 <101041058+Dharam8218@users.noreply.github.com>
2025-02-13 13:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var isDate = function (input) {
   //   write your code here
+	return (^[0-9-]*$.test(input));
 };
+
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
+
